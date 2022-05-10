5e254f26951fbdaedc0432914daa053bfe559204
fnanoia <francisconanoia@hotmail.com>
2022-05-10 09:45:32 -0300

    librerias up

--- app/main.js
+++ app/main.js
@@ -8,70 +8,45 @@ let checkoutButtons;
 let carritoStorage;
 
 let lista = document.getElementById("pushear__items__catalogo");
-function renderItem (array) {
+function renderItemHome (array) {
     for (const producto of array){
         let listaCatalogo = document.createElement("div");
         listaCatalogo.innerHTML = `
         <h3 class="render__div">${producto.nombre}</h3>
         <img src="${producto.img}">
         <p class="render__div">$${producto.precio}</p>
-        <button class="boton__agregar" id="${producto.id}">Agregar</button>
-        `
+        <button class="boton__agregar" id="${producto.id}">Agregar</button>`
         ;
         lista.appendChild(listaCatalogo);
         lista.classList.add("producto__catalogo");
         }
 }
 
-//Catalogo en json, busca los productos y renderiza la pagina de inicio de la tienda, eventos de clicks
+//Fetch de JSON estatico, renderiza la pagina de inicio de la tienda
+function fetchData () {
 fetch("json/catalogo.json")
   .then((resp) => resp.json())
   .then((data) => {
   
   catalogoJSON = data;
-  renderItem(catalogoJSON);
+  renderItemHome(catalogoJSON);
    
 botonAgregar = document.querySelectorAll(".boton__agregar");
 for (const boton of botonAgregar) {
     boton.addEventListener("click", agregarItem);
     boton.addEventListener("click", () =>{
-        Toastify({
-            text: "Agregado al carrito",
-            duration: 1000,
-            gravity: "top", 
-            position: "right", 
-            backgroundColor: "rgb(223, 46, 46)"
-          }).showToast();
+    toastAdd();
     })
 }
 })
 .catch(console.log("Error"));
+}
+fetchData();
 
 //Recargar Productos. ONCLICK
 document.getElementById("productos__render").addEventListener("click", () =>{
-
-fetch("json/catalogo.json")
-  .then((resp) => resp.json())
-  .then((data) => {
-  
-  catalogoJSON = data;
-  renderItem(catalogoJSON);
-   
-botonAgregar = document.querySelectorAll(".boton__agregar");
-for (const boton of botonAgregar) {
-    boton.addEventListener("click", agregarItem);
-    boton.addEventListener("click", () =>{
-        Toastify({
-            text: "Agregado al carrito",
-            duration: 1000,
-            gravity: "top", 
-            position: "right", 
-            backgroundColor: "rgb(223, 46, 46)"
-          }).showToast();
-    })
-}
-})
- lista.innerHTML = "";
+    fetchData();
+    lista.innerHTML = "";
 });
 
 let compras = document.getElementById("pushear__compras");
@@ -95,29 +70,23 @@ document.getElementById("search__item__button").addEventListener("click", functi
     lista.innerHTML = "";
 
     if(searchedItem.length > 0){
-            renderItem(searchedItem);
+            renderItemHome(searchedItem);
             botonAgregar = document.querySelectorAll(".boton__agregar");
             for (const boton of botonAgregar) {
                 boton.addEventListener("click", agregarItem);
                 boton.addEventListener("click", () =>{
-                    Toastify({
-                        text: "Agregado al carrito",
-                        duration: 1000,
-                        gravity: "top",
-                        position: "right", 
-                        backgroundColor: "rgb(223, 46, 46)",
-                      }).showToast();
+                toastAdd();
                 })}  
     }else{lista.innerHTML = `<div class="alert__no__results">No existen resultados para su busqueda</div>`};
 });
 
-//Side bar
+//Side bar menu
 let sideBarButton = document.querySelector(".sidebar__button");
 sideBarButton.addEventListener("click", () => {
     document.getElementById("menu").classList.toggle("sidebar__button__mostrar");
 })
 
-//Recargar carrito local storage.Incluye Checkout(a desarrollar)
+//Render Carrito local storage. Incluye Checkout(a desarrollar)
 let carritoCounter = document.getElementById("carrito__contador");
 carritoCounter.addEventListener("click", renderItemCarrito);
 carritoCounter.addEventListener("click", renderCheckout);
@@ -126,14 +95,12 @@ function renderItemCarrito () {
     lista.innerHTML = "";
     carritoStorage = JSON.parse(localStorage.getItem("CarritoStorage")) || [];
 
-    
     for (const producto of carritoStorage){
         let listaCatalogo = document.createElement("div");
         listaCatalogo.innerHTML = `
         <h3 class="render__div">${producto.nombre}</h3>
         <img src="${producto.img}">
-        <p class="render__div">$${producto.precio}</p>
-        `
+        <p class="render__div">$${producto.precio}</p>`
         ;
         lista.appendChild(listaCatalogo);
         lista.classList.add("producto__catalogo");
@@ -149,13 +116,12 @@ function renderCheckout () {
         checkoutButtons.innerHTML = `
         <button id="limpiar__carrito">Limpiar carrito</button>
         <button id="terminar__carrito">Terminar compra</button>
-        <div id="total__carrito"></div>
-        `;
-        
+        <div id="total__carrito"></div>`
+        ;
+
         checkoutButtons.setAttribute("id", "div__checkout");
         lista.appendChild(checkoutButtons);
 
-    
     document.getElementById("limpiar__carrito").addEventListener("click", () =>{
         lista.innerHTML= "";
         compras.innerHTML = "";
@@ -164,13 +130,7 @@ function renderCheckout () {
     });
 
     document.getElementById("terminar__carrito").addEventListener("click", () =>{
-        Swal.fire({
-            text: "Desea continuar?",
-            showConfirmButton: true,
-            showDenyButton: true,
-            confirmButtonText: "Comprar!",
-            denyButtonText: "Cancelar",
-              });          
+      swalCheckout();
     });
 
     total = document.getElementById("total__carrito");
@@ -181,6 +141,7 @@ function renderCheckout () {
 
     total.appendChild(totalAgregado);
     total.classList.add("total__agregado");
+
     }else{lista.innerHTML= `<div class="alert__no__results">Aun no se han agregado productos al carrito</div>`}
 }
 
